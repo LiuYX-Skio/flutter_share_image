9994c24c4518bb46539689fb6fb45efee94a68d5
liuyx <ex_jian.liu@avatr.com>
2024-11-29 13:45:37 +0800

    外接纹理测试

--- android/src/main/kotlin/com/starry/shareimage/FlutterShareImagePlugin.kt
+++ android/src/main/kotlin/com/starry/shareimage/FlutterShareImagePlugin.kt
@@ -2,13 +2,23 @@ package com.starry.shareimage
 
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.view.Surface
+import android.view.TextureView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.transition.Transition
+import io.flutter.embedding.android.FlutterTextureView
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.view.TextureRegistry
+import com.bumptech.glide.request.target.SimpleTarget
+
+
+
 
 
 class FlutterShareImagePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
@@ -37,22 +47,38 @@ class FlutterShareImagePlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
     }
 
     /**
-     *
      * 加载图片并生成纹理
      * */
     private fun loadImageTexture(result: MethodChannel.Result) {
-        val bitmap = BitmapFactory.decodeResource(context?.resources, R.drawable.img_test)
-        textureRegistry?.apply {
-            val surface = Surface(surfaceTexture?.surfaceTexture()?.apply {
-                setDefaultBufferSize(bitmap.width, bitmap.height)
-            })
-            val rect = Rect(0, 0, bitmap.width, bitmap.height)
-            val canvas = surface.lockCanvas(rect)
-            canvas.drawBitmap(bitmap, rect, rect, null)
-            bitmap.recycle()
-            surface.unlockCanvasAndPost(canvas)
-        }
-        result.success(textureId)
+        Glide.with(context!!).asBitmap().load("https://img2.baidu.com/it/u=3565369971,2082314928&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1732986000&t=08598d4ee0a5b5ca647292a9be2b9b1d").into(object : SimpleTarget<Bitmap?>() {
+            override fun onResourceReady(bitmap: Bitmap, transition: Transition<in Bitmap?>?) {
+                textureRegistry?.apply {
+                    val surface = Surface(surfaceTexture?.surfaceTexture()?.apply {
+                        setDefaultBufferSize(bitmap.width, bitmap.height)
+                    })
+                    val rect = Rect(0, 0, bitmap.width, bitmap.height)
+                    val canvas = surface.lockCanvas(rect)
+                    canvas.drawBitmap(bitmap, rect, rect, null)
+                    bitmap.recycle()
+                    surface.unlockCanvasAndPost(canvas)
+                }
+                result.success(textureId)
+
+            }
+        }) //方法中设置<span style="font-family: Arial, Helvetica, sans-serif;">asBitmap可以设置回调类型</span>
+
+//        val bitmap = BitmapFactory.decodeResource(context?.resources, R.drawable.img_test)
+//        textureRegistry?.apply {
+//            val surface = Surface(surfaceTexture?.surfaceTexture()?.apply {
+//                setDefaultBufferSize(bitmap.width, bitmap.height)
+//            })
+//            val rect = Rect(0, 0, bitmap.width, bitmap.height)
+//            val canvas = surface.lockCanvas(rect)
+//            canvas.drawBitmap(bitmap, rect, rect, null)
+//            bitmap.recycle()
+//            surface.unlockCanvasAndPost(canvas)
+//        }
+//        result.success(textureId)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
